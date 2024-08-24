e7e241a1b635f288fbcec630629a5e23cb7aebd9
wesley-goyette <wesley.goyette@twostory.com>
2024-08-23 22:18:25 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/wesdesk_v2/',
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
