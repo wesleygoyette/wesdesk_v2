f07a130525fa20ca0f0c8b539b31539d57f71612
wesley-goyette <wesley.goyette@twostory.com>
2024-08-27 16:02:10 -0500

    remove base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/wesdesk_v2/',
     plugins: [vue()],
     resolve: {
         alias: {
